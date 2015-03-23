154d78ca12ae4f76dc92dd1f0eb6b265fa738e3f
Sean Grimes <sean.grimes@praeses.com>
2015-03-23 17:00:07 -0500

    Adding web project skeleton
    This project will serve up our HTML5 SPA using Aurelia, host our
    SignalR hubs for communicating events to the web browser in real time,
    and contain our API proxy that will convert REST calls to messages
    on the service bus.

---
+++ MunkeyIssues.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MunkeyIssues.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
