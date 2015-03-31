1d4ac0c829dbac5ced6d601415b4c34f6e8fc5bb
Sean Grimes <sean.grimes@praeses.com>
2015-03-31 09:06:21 -0500

    Adding migrations project using FluentMigrator
    Todo: Make sure EF Fluent Migrations are disabled, add an initial
    migration that will create all the current tables in the database,
    add a migration that will seed some initial data

---
+++ MunkeyIssues.Migrations/UserService/2015/03/AddSaltColumnToUsersTable.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+
+namespace MunkeyIssues.Migrations.UserService._2015._03
+{
+    [Tags("Production", "Integration")]
+    [Migration(20150331085701)]
+    public class AddSaltColumnToUsersTable : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Column("Salt").OnTable("Users").AsString(255).NotNullable();
+        }
+    }
+}
